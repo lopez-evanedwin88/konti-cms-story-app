148a11b2247d3291afb15d84b01c3b4b3e373c8f
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2023-08-04 20:47:50 +0800

    VIEW: Added API backbone

---
+++ client/composables/apiFetch.ts
@@ -0,0 +1,16 @@
+import type { FetchOptions } from "ohmyfetch";
+
+export const useApiFetch = (path: string, opts?: FetchOptions) => {
+  const config = useRuntimeConfig();
+
+  return $fetch(path, {
+    baseURL: process.server
+      ? config.public.BASE_API_BROWSER_URL
+      : config.public.BASE_API_URL,
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    },
+    ...(opts && { ...opts }),
+  });
+};
