c9e58d493c4bf08e969a483c1461867492016b2f
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2023-08-04 20:47:00 +0800

    VIEW: Nuxt config file

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
+        }
+      ]
+    },
+  },
+  runtimeConfig: {
+    // Config within public will be also exposed to the client
+    public: {
+      BASE_API_URL: process.env.API_URL_SERVER,
+      BASE_API_BROWSER_URL: process.env.API_URL_BROWSER
+    }
+  }
 })
