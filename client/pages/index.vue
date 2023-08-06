b5910a010cb179cd30d5078cbfa418b538e01b15
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2023-08-06 13:39:38 +0800

    VIEW: Modify text homepage

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="section">
       <div class="container">
-        <h1 class="title">Konti-CMS Story Management</h1>
+        <h1 class="title">Welcome Konti-CMS Story Management!</h1>
       </div>
     </section>
   </template>
